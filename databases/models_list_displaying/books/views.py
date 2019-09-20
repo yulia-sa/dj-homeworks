151296fce95f4ebf53c0287ca90aaf888a5df051
Yulia Savanova <sav.jul@gmail.com>
2019-09-20 03:06:50 +0300

    Homework-3 ex-3 without pagination

--- databases/models_list_displaying/books/views.py
+++ databases/models_list_displaying/books/views.py
@@ -1,7 +1,37 @@
+from datetime import datetime
+
 from django.shortcuts import render
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+
+    books = Book.objects.all()
+
+    context = {
+        'books': books
+    }
+
+    return render(request, template, context)
+
+
+def date_books_view(request, year, month, day):
+    template = 'books/books_list.html'
+
+    try:
+        date = datetime(year=year, month=month, day=day).date()
+    except (ValueError, TypeError):
+        date = ''
+
+    books = Book.objects.filter(pub_date=date)
+
+    print(books)
+    
+
+    context = {
+        'books': books
+    }
+
     return render(request, template, context)
--- databases/models_list_displaying/main/urls.py
+++ databases/models_list_displaying/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, date_books_view
 
 urlpatterns = [
     path('', books_view, name='books'),
+    path('books/', books_view, name='books'),
+    path('books/<int:year>-<int:month>-<int:day>/', date_books_view, name='date'),
     path('admin/', admin.site.urls),
 ]

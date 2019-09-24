aec85a223ea800ea9ea2b1e51a05825a3ad064d7
Yulia Savanova <sav.jul@gmail.com>
2019-09-24 03:59:02 +0300

    Homework-4 ex-2 --> done

--- databases_2/orm_query/articles/views.py
+++ databases_2/orm_query/articles/views.py
@@ -6,10 +6,10 @@ from .models import Article
 
 def articles_list(request):
     template_name = 'articles/news.html'
-    context = {}
+    object_list = Article.objects.all().select_related('author', 'genre').only('author__name', 'genre__name').order_by('-published_at')
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    context = {
+        'object_list': object_list
+    }
 
     return render(request, template_name, context)

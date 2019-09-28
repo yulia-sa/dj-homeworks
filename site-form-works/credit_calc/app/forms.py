4aefdd24f8eebd7da3d694ecbbb9a4c7c4b15cc4
Yulia Savanova <sav.jul@gmail.com>
2019-09-28 07:05:24 +0300

    Homework-5 ex-1 --> done

--- site-form-works/credit_calc/app/forms.py
+++ site-form-works/credit_calc/app/forms.py
@@ -2,16 +2,32 @@ from django import forms
 
 
 class CalcForm(forms.Form):
-    initial_fee = forms.IntegerField(label="Стоимость товара")
-    rate = forms.CharField(label="Процентная ставка")
-    months_count = forms.IntegerField(label="Срок кредита в месяцах")
+    initial_fee = forms.IntegerField(label="Стоимость товара", required=True)
+    rate = forms.IntegerField(label="Процентная ставка", required=True)
+    months_count = forms.IntegerField(label="Срок кредита в месяцах", required=True)
+
 
     def clean_initial_fee(self):
         # валидация одного поля, функция начинающаяся на `clean_` + имя поля
         initial_fee = self.cleaned_data.get('initial_fee')
-        if not initial_fee or initial_fee < 0:
+        if initial_fee < 0:
             raise forms.ValidationError("Стоимость товара не может быть отрицательной")
         return initial_fee
+
+
+    def clean_rate(self):
+        rate = self.cleaned_data.get('rate')
+        if rate < 0:
+            raise forms.ValidationError("Процентная ставка не может быть отрицательной")
+        return rate
+
+
+    def clean_months_count(self):
+        months_count = self.cleaned_data.get('months_count')
+        if months_count <= 0:
+            raise forms.ValidationError("Срок кредита в месяцах должен быть больше нуля")
+        return months_count
+
 
     def clean(self):
         # общая функция валидации
--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -6,9 +6,20 @@ from .forms import CalcForm
 def calc_view(request):
     template = "app/calc.html"
 
-    form = CalcForm
+    form = CalcForm(request.GET)
+
     context = {
-        'form': form
+        'form': form  
     }
+
+    if form.is_valid():
+        common_result = form.cleaned_data['initial_fee'] + \
+                        form.cleaned_data['initial_fee'] * \
+                        form.cleaned_data['rate'] * 0.01
+
+        result = common_result / form.cleaned_data['months_count']
+
+        context['common_result'] = common_result
+        context['result'] = result
 
     return render(request, template, context)
